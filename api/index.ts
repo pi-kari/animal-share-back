254d6cbf67140f929a6ee629eae7705c606a564e
pi-kari <79442987+pi-kari@users.noreply.github.com>
2025-08-10 13:24:05 +0900

    fix

--- api/index.ts
+++ api/index.ts
@@ -17,5 +17,5 @@ app.get("/", (req, res) => {
   res.send("This is Animal Share Backend Server");
 });
 registerRoutes(app);
-
-export default app;
+app.listen(3000, () => console.log("Server ready on port 3000."));
+module.exports = app;
