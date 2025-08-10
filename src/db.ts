d2f36299bfd0a54972846cb714c2e2e05d09b071
pi-kari <79442987+pi-kari@users.noreply.github.com>
2025-08-10 12:43:18 +0900

    fix

--- src/db.ts
+++ src/db.ts
@@ -1,18 +1,10 @@
-// import { Pool, neonConfig } from "@neondatabase/serverless";
-// import { drizzle } from "drizzle-orm/neon-serverless";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 import ws from "ws";
 import * as schema from "./schema";
-import { Pool } from "pg";
 
 require("dotenv").config({ path: ".env" });
-// neonConfig.webSocketConstructor = ws;
+neonConfig.webSocketConstructor = ws;
 
-export const pool = new Pool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASS,
-  port: Number(process.env.DATABASE_PORT),
-  database: process.env.DATABASE_DB,
-});
-export const db = drizzle(pool, { schema });
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const db = drizzle({ client: pool, schema });
