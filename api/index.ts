1657519db14f4ddcd0d49a7c61fc62ff2902e25d
pi-kari <79442987+pi-kari@users.noreply.github.com>
2025-08-10 13:29:59 +0900

    fix

--- api/index.ts
+++ api/index.ts
@@ -5,6 +5,7 @@ import { registerRoutes } from "./routes";
 require("dotenv").config({ path: ".env" });
 
 const app = express();
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
