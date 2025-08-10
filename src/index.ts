01198ced099ffd3a295f97ab440bf0f5f5e3e00a
pi-kari <79442987+pi-kari@users.noreply.github.com>
2025-08-10 12:57:04 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,16 @@ import cors from "cors";
 import { registerRoutes } from "./routes";
 
 require("dotenv").config({ path: ".env" });
-const port = Number(process.env.BACKEND_PORT);
 
-async function main() {
-  const app = express();
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(
-    cors({
-      origin: process.env.FRONTEND_URL,
-      credentials: true,
-    })
-  );
-  registerRoutes(app);
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+registerRoutes(app);
 
-  app.listen(port, async () => {
-    console.log(`Server is running on port ${port}`);
-  });
-}
-
-main();
+export default app;
