79fc9717c176f4d334b50d89f9b3e78d9a3f1b51
pi-kari <79442987+pi-kari@users.noreply.github.com>
2025-08-11 23:34:24 +0900

    fix

--- api/index.ts
+++ api/index.ts
@@ -8,21 +8,20 @@ dotenv.config({ path: ".env" });
 const port = Number(process.env.BACKEND_PORT);
 export const multerMemoryStorage = multer({ storage: multer.memoryStorage() });
 
-async function main() {
-  const app = express();
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(
-    cors({
-      origin: process.env.FRONTEND_URL ?? "http://localhost:3000/",
-      credentials: true,
-    })
-  );
-  registerRoutes(app);
+const app = express();
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL ?? "http://localhost:3000/",
+    credentials: true,
+  })
+);
+registerRoutes(app);
 
-  app.listen(port, async () => {
-    console.log(`Server is running on port ${port}`);
-  });
-}
+app.listen(port, async () => {
+  console.log(`Server is running on port ${port}`);
+});
 
-main();
+export default app;
